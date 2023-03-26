35ca2be58e4f6fc95747f389d64b60299fe0e6cb
Thanseeh Rahman <105215750+thanseehrahman@users.noreply.github.com>
2023-03-26 15:00:10 +0530

    Update title for App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Git
+          React Git Pull
         </p>
       </header>
     </div>
